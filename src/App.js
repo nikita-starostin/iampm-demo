7af37e45b45ee7a1a7c11bb9ab3cbe5a0c379370
nikita starostin <n.starostin@itransition.com>
2023-10-28 13:13:59 +0300

    add new button to show name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [name, setName] = useState('');
+    const changeName = () => {
+        const newName = prompt('What is your name?');
+        setName(newName);
+    }
+
     return (
         <div className="App">
             <h1>I AM PM</h1>
-            <h2 style={{color: 'red'}}>I love pony</h2>
+            {name && <h2>Hello <b>{name}</b></h2>}
+            <button onClick={changeName}>Change name</button>
         </div>
     );
 }
