6c16b7477ab61f113a9e0eedf6793118e5e2f92d
nikita starostin <n.starostin@itransition.com>
2023-10-28 13:01:45 +0300

    add new string and remove not used styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>I AM PM</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>I AM PM</h1>
+            <h2 style={{color: 'red'}}>I love pony</h2>
+        </div>
+    );
 }
 
 export default App;
